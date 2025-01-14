c96cda922f09c77328424de0263c5c96e759959b
Kirill Belov <kirillbelovtest@gmail.com>
2025-01-14 07:37:05 +0400

    test

---
+++ javaSource/main/kirillbelov/websocketlink/SocketIOClient.java
@@ -0,0 +1,14 @@
+package kirillbelov.websocketlink;
+
+import io.socket.client.*;
+
+public class SocketIOClient {
+
+    public Socket ioSocket; 
+
+    public LTPClient ltpClient; 
+
+    public SocketIOClient(String host, int port, int redirectPort) {
+        this.ioSocket = new Socket()
+    }
+}
